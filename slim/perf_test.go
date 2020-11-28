78a1d4bb8ec90c3fb53e7dd002af6c6660d261c6
Leonardo Di Donato <leodidonato@gmail.com>
2020-11-28 21:11:41 +0100

    new(slim): benchmarks
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ slim/perf_test.go
@@ -0,0 +1,88 @@
+package slim
+
+import (
+	"testing"
+
+	"github.com/leodido/go-conventionalcommits"
+	cctesting "github.com/leodido/go-conventionalcommits/testing"
+)
+
+// Avoid compiler optimizations that could remove the actual call we are benchmarking during benchmarks
+var benchParseResult conventionalcommits.Message
+
+type benchCase struct {
+	input []byte
+	label string
+}
+
+var benchCases = []benchCase{
+	{
+		label: "[ok] minimal",
+		input: []byte("fix: x"),
+	},
+	{
+		label: "[ok] minimal with scope",
+		input: []byte("fix(s): x"),
+	},
+	{
+		label: "[ok] minimal breaking with scope",
+		input: []byte("fix(s)!: x"),
+	},
+	{
+		label: "[ok] full with 50 characters long description",
+		input: []byte("fix(s)!: abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwx"),
+	},
+	{
+		label: "[no] empty",
+		input: []byte(""),
+	},
+	{
+		label: "[no] type but missing colon",
+		input: []byte("fix"),
+	},
+	{
+		label: "[no] type but missing description",
+		input: []byte("feat: "),
+	},
+	{
+		label: "[no] type and scope but missing description",
+		input: []byte("feat(scope): "),
+	},
+	{
+		label: "[no] breaking with type and scope but missing description",
+		input: []byte("feat(scope): "),
+	},
+	{
+		// ~~ means it returns the conventionalcommits.Message instance (description cut before the newline) and the error
+		label: "[~~] newline in description",
+		input: []byte("feat(scope): new\x0Aline"),
+	},
+	{
+		label: "[no] missing whitespace in description",
+		input: []byte("feat(scope):a"),
+	},
+}
+
+func BenchmarkSlimParseMinimalTypes(b *testing.B) {
+	for _, tc := range benchCases {
+		tc := tc
+		m := NewMachine(WithBestEffort())
+		b.Run(cctesting.RightPad(tc.label, 50), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				benchParseResult, _ = m.Parse(tc.input)
+			}
+		})
+	}
+}
+
+func BenchmarkSlimParseConventionalTypes(b *testing.B) {
+	for _, tc := range benchCases {
+		tc := tc
+		m := NewMachine(WithBestEffort(), WithTypes(conventionalcommits.TypesConventional))
+		b.Run(cctesting.RightPad(tc.label, 50), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				benchParseResult, _ = m.Parse(tc.input)
+			}
+		})
+	}
+}
--- testing/testing.go
+++ testing/testing.go
@@ -1,6 +1,16 @@
 package testing
 
+import (
+	"strings"
+)
+
 // StringAddress returns the address of the input string.
 func StringAddress(str string) *string {
 	return &str
+}
+
+// RightPad pads a string with spaces until the given limit, or it cuts the string to the given limit.
+func RightPad(str string, limit int) string {
+	str = str + strings.Repeat(" ", limit)
+	return str[:limit]
 }
