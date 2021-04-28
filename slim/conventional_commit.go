fd6155323fd315172efc0ec51958f67a5d821289
Leonardo Di Donato <leodidonato@gmail.com>
2021-04-29 00:01:29 +0200

    new: footers field in the conventional commit struct + trim suffix blank line from its body part
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- slim/conventional_commit.go
+++ slim/conventional_commit.go
@@ -4,13 +4,13 @@ import (
 	"github.com/leodido/go-conventionalcommits"
 )
 
-
 type conventionalCommit struct {
 	_type       string
 	descr       string
 	scope       string
 	exclamation bool
 	body        string
+	footers     map[string][]string
 }
 
 func (c *conventionalCommit) minimal() bool {
@@ -26,7 +26,14 @@ func (c *conventionalCommit) export() conventionalcommits.Message {
 		out.Scope = &c.scope
 	}
 	if c.body != "" {
+		// Trim suffix blank line
+		if len(c.body) >= 2 && c.body[len(c.body)-2:] == "\n\n" {
+			c.body = c.body[:len(c.body)-2]
+		}
 		out.Body = &c.body
+	}
+	if len(c.footers) > 0 {
+		out.Footers = c.footers
 	}
 
 	return out
